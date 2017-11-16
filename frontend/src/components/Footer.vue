2de2264ad5ed2ede460b5f9624ccda140eeded8a
Marani Matias Ezequiel <maranimatias@gmail.com>
2017-11-16 01:48:26 -0300

    base layout

---
+++ frontend/src/components/Footer.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+  v-footer(app)
+    span &copy; 2017 {{ msg }}
+</template>
+
+<script>
+export default {
+  name: 'footer',
+  data() {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ frontend/src/components/Toolbar.vue
@@ -0,0 +1,28 @@
+<template lang="pug">
+  v-toolbar(app)
+    v-toolbar-side-icon(@click.stop='drawerClick()')
+    v-toolbar-title(v-text='title')
+    v-spacer
+</template>
+
+<script>
+export default {
+  name: 'toolbar',
+  data() {
+    return {
+      miniVariant: false,
+      rightDrawer: false,
+      title: 'Vuetify.js'
+    }
+  },
+  methods: {
+    drawerClick: function () {
+      this.$emit('drawer');
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
