9ed8b6bc4f2250e3d613582a4515ddb1586361d6
Marani Matias Ezequiel <maranimatias@gmail.com>
2018-01-26 11:55:24 -0300

    Delete old file to frontend path

--- frontend/src/store/getters.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-export default {
-  findMovie: (state) => (movieId) => {
-    return state.discover.results.find((movie) => movie.id === movieId);
-  }
-};
--- frontend/src/store/mutations.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-export default {
-  set_discover_movie: (state, discover) => {
-    state.discover = discover;
-  }
-};
