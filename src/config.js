f7331542c7cc7264ea0c44c93f48ad356c5a5884
Marani Matias Ezequiel <maranimatias@gmail.com>
2018-03-01 20:25:20 -0300

    Auto set baseURL

--- src/config.js
+++ src/config.js
@@ -1,5 +1,11 @@
+let baseURL = "http://localhost:3000/api";
+
+if (window.cordova) {
+  baseURL = "http://192.168.1.6:3000/api";
+  // baseURL = "http://nodejs-mongo-persistent-ttads-handball.a3c1.starter-us-west-1.openshiftapps.com/api"
+}
+
 export default {
   productionTip: true,
-  // baseURL: "http://localhost:3000/api",
-  baseURL: "http://192.168.1.6:3000/api",
+  baseURL
 };
