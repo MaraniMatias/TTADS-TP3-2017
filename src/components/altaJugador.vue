9639dd6809a719c76da4d2c81ef0f8f27b194406
Marani Matias Ezequiel <maranimatias@gmail.com>
2018-03-06 01:37:59 -0300

    Add altaJugador pag

---
+++ src/components/altaJugador.vue
@@ -0,0 +1,94 @@
+<template>
+<v-ons-page>
+  <v-ons-toolbar>
+    <div class="left">
+      <v-ons-toolbar-button @click="$router.back()" icon="ion-ios-arrow-back, material:ion-android-arrow-back"></v-ons-toolbar-button>
+    </div>
+    <div class="center">Alta de Jugador</div>
+  </v-ons-toolbar>
+
+  <v-ons-list>
+    <v-ons-list-item :modifier="md ? 'nodivider' : ''">
+      <label class="center">
+        <v-ons-input float maxlength="60" required type="text"
+          placeholder="Nombre" v-model="entidad.nombre">
+        </v-ons-input>
+      </label>
+    </v-ons-list-item>
+    <v-ons-list-item :modifier="md ? 'nodivider' : ''">
+      <label class="center">
+        <v-ons-input float maxlength="60" required type="text"
+          placeholder="Apellido" v-model="entidad.apellido">
+        </v-ons-input>
+      </label>
+    </v-ons-list-item>
+
+    <v-ons-list-item :modifier="md ? 'nodivider' : ''">
+      <label class="center">
+        <v-ons-input float type="numeric"
+          placeholder="Peso" v-model="entidad.peso">
+        </v-ons-input>
+      </label>
+    </v-ons-list-item>
+    <v-ons-list-item :modifier="md ? 'nodivider' : ''">
+      <label class="center">
+        <v-ons-input float type="numeric"
+          placeholder="Altura" v-model="entidad.altura">
+        </v-ons-input>
+      </label>
+    </v-ons-list-item>
+    <v-ons-list-item :modifier="md ? 'nodivider' : ''">
+      <label class="center">
+        <v-ons-input float type="numeric"
+          placeholder="Edad" v-model="entidad.edad">
+        </v-ons-input>
+      </label>
+    </v-ons-list-item>
+    <v-ons-list-item>
+      <v-ons-button modifier="large" style="margin: 0px 6px 10px 0px" :disabled="disabledBtn" @click="submit()">
+        <span v-if="!isLoading">Guardar</span>
+        <v-ons-icon icon="ion-load-c" spin size="26px" v-else></v-ons-icon>
+      </v-ons-button>
+    </v-ons-list-item>
+  </v-ons-list>
+
+</v-ons-page>
+</template>
+
+<script>
+export default {
+  name: 'altaJugador',
+  components: {},
+  data() {
+    return {
+      entidad: {
+        nombre: '',
+        apellido: '',
+        peso: 63,
+        altura: 160,
+        edad: 27,
+      },
+    };
+  },
+  methods: {
+    submit() {
+      console.log(":D");
+    }
+  },
+  mounted() {}
+};
+</script>
+
+<style scoped>
+ons-list {
+  padding-top: 12px;
+}
+
+ons-input {
+  width: 100%;
+}
+
+.page__content {
+  background-color: #fff !important;
+}
+</style>
